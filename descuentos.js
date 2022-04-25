432560719b5c6d06942bd9296e95a6e2e50eb62f
Sebas <sebaslg96@gmail.com>
2022-04-25 12:11:43 -0500

    terminada modificaciones clase de descuentos

---
+++ descuentos.js
@@ -0,0 +1,28 @@
+const precioOriginal = 120;
+const descuento = 18;
+
+function calcularPrecioConDescuento(precio, descuento){
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+    return precioConDescuento;
+}
+
+function onClickButtonPriceDiscount(){
+    const inputPrice = document.getElementById("inputPrice");
+    const priceValue = inputPrice.value;
+    const inputDiscount = document.getElementById("inputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+    const resultP = document.getElementById("resultP");
+    resultP.innerText = "El precio con descuento son: " + precioConDescuento + " USD";
+}
+
+/*
+console.log({
+    precioOriginal,
+    descuento,
+    porcentajePrecioConDescuento,
+    precioConDescuento
+}); */
+
