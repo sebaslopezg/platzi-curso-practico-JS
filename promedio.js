0b7115a36419afd74b1510f1572bd6e29c0daa05
Sebas <sebaslg96@gmail.com>
2022-04-30 19:51:58 -0500

    finalizado clase de promedios

---
+++ promedio.js
@@ -0,0 +1,26 @@
+const lista1 = [
+    100,
+    200,
+    300,
+    500
+];
+
+
+
+ function calcularMediaAritmetica(lista){
+//     let sumaLista = 0;
+
+//     for(let i = 0; i< lista.length; i++){
+//         sumaLista = sumaLista + lista[i];
+//     }
+
+const sumaLista = lista.reduce(
+    function(valorAcomulado = 0, nuevoElemento){
+        return valorAcomulado + nuevoElemento;
+    }
+);
+    
+    const promedioLista = sumaLista / lista.length;
+    return promedioLista;
+ }
+
