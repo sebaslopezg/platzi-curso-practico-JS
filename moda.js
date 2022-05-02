c62c7a85272ec2a1a6ffa72d5dc7b2a3126fdc53
Sebas <sebaslg96@gmail.com>
2022-05-02 09:55:41 -0500

    terminado reto meter moda.js en una funcion

--- moda.js
+++ moda.js
@@ -1,30 +1,38 @@
-const lista1 = [
-    1,
-    2,
-    3,
-    1,
-    4,
-    2,
-    2,
-    1,
-    2,
-    2,
-];
+// const lista1 = [
+//     1,
+//     2,
+//     3,
+//     1,
+//     4,
+//     2,
+//     2,
+//     1,
+//     2,
+//     2,
+// ];
 
-const lista1Count = {};
+function calcularModa(lista){
 
-lista1.map(function(elemento){
-    if(lista1Count[elemento]){
-        lista1Count[elemento] += 1; 
-    }else{
-    lista1Count[elemento] = 1;
-    } 
-});
+    const listaCount = {};
 
-const lista1Array = Object.entries(lista1Count).sort(
-    function(valorAcomulado, nuevoValor){
-        return valorAcomulado[1] - nuevoValor[1];
-    }
-);
+    lista.map(function(elemento){
+    
+        if(listaCount[elemento]){
+            listaCount[elemento] += 1; 
+        }else{
+        listaCount[elemento] = 1;
+        } 
+    
+    });
+    
+    const listaArray = Object.entries(listaCount).sort(
+        function(valorAcomulado, nuevoValor){
+            return valorAcomulado[1] - nuevoValor[1];
+        }
+    );
+    
+    const moda = listaArray[listaArray.length - 1];
 
-const moda = lista1Array[lista1Array.length - 1];
+    return moda;
+
+}
